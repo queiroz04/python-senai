8e26165dab27b4884ec9499499811abbb96cb3f3
queiroz04 <queirozjoyce04@gmail.com>
2024-12-05 09:57:13 -0300

    professor bacana

---
+++ Aula 2/aula02.py
@@ -0,0 +1,7 @@
+print('bom dia')
+nome_usuario='queiroz' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.65 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+print('seja bem-vindo', nome_usuario)
+ 
